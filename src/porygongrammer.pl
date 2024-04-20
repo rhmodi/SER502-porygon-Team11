b1f6c7569d0184fd8ed65f8e9428595f0e921b8c
AnmolNemagouda <anemagou@asu.edu>
2024-04-20 16:14:35 -0700

    added plain and remaining declarations, while and for loops, variable operation, increment, decrement and assignment, added boolean expression rules

--- src/porygongrammer.pl
+++ src/porygongrammer.pl
@@ -1,4 +1,7 @@
+%Programming block begins here
 block(block(D,E))--> ['{'],decl(D),commd(C),['}'].
+
+% Declaration statments here for constants,variables etc.
 
 decl(dec(D,DL))--> decls(D),[';'],decl(DL).
 decl(dec(D)) --> decls(D),[';'].
@@ -7,10 +10,28 @@ decls(D)--> constassign(D).
 decls(D)--> declassign(D).
 decls(D)--> plainassign(D).
 
+constassign(const(C,N)) :- ['const'], ['int'], variablename(C),[=],num(N).
+constassign(const(C,S)) :- ['const'], ['string'], variablename(C),[=],stringvalue(S).
+constassign(const(C,BE)) :- ['const'], ['bool'], variablename(C),[=],boolvalue(BE).
+constassign(const(C,F)) :- ['const'], ['float'], variablename(C),[=],floatvalue(F).
+constassign(const(C,Expr)) :- ['const'], ['int'], variablename(C),[=],expr(Expr).
+constassign(const(C,Expr)) :- ['const'], ['string'], variablename(C),[=],expr(Expr).
+constassign(const(C,Expr)) :- ['const'], ['bool'], variablename(C),[=],expr(Expr).
+constassign(const(C,Expr)) :- ['const'], ['float'], variablename(C),[=],expr(Expr).
 
-%% Declaration statments here
+declassign(declaration(Var,Value)) :- ['int'], variablename(Var),[=],num(Value).
+declassign(declaration(Var,Value)) :- ['string'], variablename(Var),[=],stringvalue(Value).
+declassign(declaration(Var,Value)) :- ['bool'], variablename(Var),[=],boolvalue(Value).
+declassign(declaration(Var,Value)) :- ['float'], variablename(Var),[=],floatvalue(Value).
+declassign(declaration(Var,Value)) :- ['int'], variablename(Var),[=],expr(Value).
+declassign(declaration(Var,Value)) :- ['string'], variablename(Var),[=],expr(Value). 
+declassign(declaration(Var,Value)) :- ['bool'], variablename(Var),[=],expr(Value).
+declassign(declaration(Var,Value)) :- ['float'], variablename(Var),[=],expr(Value).
 
-
+plainassign(plaindeclaration(Var)) :- ['int'], variablename(Var).
+plainassign(plaindeclaration(Var)) :- ['string'], variablename(Var).
+plainassign(plaindeclaration(Var)) :- ['bool'], variablename(Var).
+plainassign(plaindeclaration(Var)) :- ['float'], variablename(Var).
 
 % Commands List start here
 commandlist(commd(PlainCmnd,CmndList)) --> plaincommand(PlainCmnd),[;],commandlist(CmndList)
@@ -30,7 +51,7 @@ plaincommand(plain(ForInRange)) --> forinrangecommand(ForInRange).
 
 
 % Declaration of types of assignment
-assignment(assign(A))--> initialAssignment(A).
+assignment(assign(A))--> initialassignment(A).
 assignment(assign(A))--> declassign(A).
 assignment(assign(A))--> shortantAssign(A).
 
@@ -42,26 +63,9 @@ shorthandAssign(shassign(Var,Expr))--> variablename(Var),['%='],expr(Expr).
 shorthandAssign(shassign(Var,Expr))--> variablename(Var),['^='],expr(Expr).
 
 
-initialAssignment(iassign(Var,Expr))--> variablename(Var),['='],expr(Expr).
+initialassignment(iassign(Var,Expr))--> variablename(Var),['='],expr(Expr).
 
-
-% Complete other expression
-
-
-%%%% IF COMMANDS%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-ifcommand(If)--> ifpart(If).
-ifelsecommand(If,Else)--> ifpart(If),elsepart(Else).
-ifELseLaddercommand(If,Elif,Else)--> ifpart(If),elseifpart(Elif),elsepart(Else).
-
-ifpart(if(B,X))--> ['if'],['('],boolcondition(B), [')'],['{'],commandlist(X),['}'].
-elseifpart(elseif(B,E1,E2))--> ['elif'],['('],boolcondition(B), [')'],['{'],commandlist(E1),['}'],elseifpart(E2). 
-elseifpart(elseif(B,E))--> ['elif'],['('],boolcondition(B), [')'],['{'],commandlist(E),['}'].
-elsepart(else(C))--> ['else'],['{'],commandlist(C),['}'].
-
-
-%% EXPRESSIONS STARTS HERE
-
+% EXPRESSIONS STARTS HERE
 
 % Change in grammer make it left recursive
 expr(addition(X,Y))--> expr(X),['+'],term(Y).
@@ -90,7 +94,54 @@ squareRoot(sqrt(Sqrt))--> ['sqrt'],['('],expr(Sqrt),[')'].
 cube(cube(Cube))--> ['cube'],['('],expr(Cube),[')'].
 cubeRoot(cbrt(Cbrt))--> ['cbrt'],['('],expr(Cbrt),[')'].
 
+%Boolean Expressions start here including logical operators 'and', 'or', 'not'.
 
+boolcondition(or(X,Y))--> and_condition(X),['or'],boolcondition(Y).
+boolcondition(X) --> and_condition(X).
+boolcondition(and(X,Y))--> condition(X),['and'],and_condition(Y).
+and_condition(X) --> condition(X).
+condition(not(X))--> ['not'],boolcondition(X).
+condition(X)--> ['('],boolcondition(X),[')'].
+condition(InitAssign) --> initialassignment(InitAssign).
+condition(Expression)--> ['not'], expr(Expression).
+condition(equivalance(Expr1,Expr2))--> expr(Expr1),['=='],expr(Expr2).
+condition(notequalsto(Expr1,Expr2))--> expr(Expr1),['!='],expr(Expr2).
+condition(lessthan(Expr1,Expr2))--> expr(Expr1),['<'],expr(Expr2).
+condition(lessthan_orequalto(Expr1,Expr2))--> expr(Expr1),['<='],expr(Expr2).
+condition(greaterthan(Expr1,Expr2))--> expr(Expr1),['>'],expr(Expr2).
+condition(greaterthan_orequalto(Expr1,Expr2))--> expr(Expr1),['>='],expr(Expr2).
+condition(boolvalue(Bool))--> boolvalue(Bool).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% IF STATEMENT%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ifcommand(If)--> ifpart(If).
+ifelsecommand(If,Else)--> ifpart(If),elsepart(Else).
+ifELseLaddercommand(If,Elif,Else)--> ifpart(If),elseifpart(Elif),elsepart(Else).
+
+ifpart(if(B,X))--> ['if'],['('],boolcondition(B), [')'],['{'],commandlist(X),['}'].
+elseifpart(elseif(B,E1,E2))--> ['elif'],['('],boolcondition(B), [')'],['{'],commandlist(E1),['}'],elseifpart(E2). 
+elseifpart(elseif(B,E))--> ['elif'],['('],boolcondition(B), [')'],['{'],commandlist(E),['}'].
+elsepart(else(C))--> ['else'],['{'],commandlist(C),['}'].
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% WHILE STATEMENT%%%%%%%%%%%%%%%%%%%%%%%%%%
+whilecommand(Condition)--> ['while'],['('],boolcondition(Condition), [')'],['{'],commandlist(C),['}'].
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% FOR STATEMENT%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for_command(For)--> ['for'],['('],assignment(Assign),[';'],boolcondition(BoolCondition),[';'],variableupdation(Varupdation),[')'],['{'],commandlist(C),['}'].
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% variable updation%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+variableupdation(increment(Ops))--> increment_operation(Ops).
+variableupdation(decrement(Ops))--> decrement_operation(Ops).
+variableupdation(assignment(Assign))--> expr(Assign)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% increment and decrement operations%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+increment_operation(increment(Var)) --> variablename(Var),['++'].
+increment_operation(increment(Var)) --> ['++'],variablename(Var).
+
+decrement_operation(decrement(Var)) --> variablename(Var),['--'].
+decrement_operation(decrement(Var)) --> ['--'],variablename(Var).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% FOR IN RANGE STATEMENT%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+forinrangecommand(For)--> ['for'],['('],assignment(Assign),['in'],range(Range),[')'],['{'],commandlist(C),['}'].
 
 
