7de5840933a3643060e24ff324a834f92b485ef9
rakshil14-2 <56144321+rakshil14-2@users.noreply.github.com>
2024-04-28 18:26:44 -0700

    comments removed

--- src/porygongrammer.pl
+++ src/porygongrammer.pl
@@ -192,18 +192,5 @@ restOfVariableName([Char|Rest]):-
 restOfVariableName([]).
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ALPHANUMERIC DEFINITION STATEMENT%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%alphanumeric(Alpha) --> character(Alpha), alphanumeric(Alpha).
-%alphanumeric(Alpha) --> character(Alpha).
-%character(Char) --> letter(Char).
-%character(Char) --> num(Char).
-%character(Char) --> special_char(Char).
 
 num(num(Num)) --> [Num],{number(Num)}.
-
-%letter(Letter) --> [Letter],lowercase_letter(Letter).
-%letter(Letter) --> [Letter],uppercase_letter(Letter).
-%lowercase_letter(Lower)--> {member(Lower, ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])}.
-%uppercase_letter(Upper) --> {member(Upper, ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'])}.
-%digit(Digit) --> {member(Digit, [0,1,2,3,4,5,6,7,8,9])}.
-%special_char(Special) --> {member(Special,  ['!','@','#','$','%','^','&','*','(',')','_','-','+','=','{','}','',']','|',':',';','"','<','>',',','.','?','/'])}.
